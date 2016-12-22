e91d98c79b9c7292b7bbc2f67b570723a05f73a3
joshmcintyre <josh@jmcintyre.net>
2016-12-21 22:39:06 -0500

    Add a suggestions url to the API to allow retrieval of search suggestions by key

--- src/api/trainingreference.py
+++ src/api/trainingreference.py
@@ -19,6 +19,7 @@ web.config.debug = True
 urls = (
 	"/", "trainingreference",
 	"/(programs|exercises)", "all",
+	"/(programs|exercises)/suggestions/(.*)", "suggest",
 	"/(programs|exercises)/(.*)/(.*)", "query"
 	)
 
@@ -69,7 +70,7 @@ class trainingreference:
 #
 class all:
 
-	# This function handles GET requests for programs
+	# This function handles GET requests for programs and exercises
 	#
 	# Argument: collection
 	# Return: response
@@ -91,9 +92,34 @@ class all:
 # This class handles requests to query the database
 #
 #
+class suggest:
+
+	# This function handles GET requests for programs and exercises
+	#
+	# Argument: collection
+	# Argument: key
+	# Return: response
+	#
+	#
+	def GET(self, collection, key):
+
+		web.header("Content-Type", "text/html")
+
+		results = query_database(collection.title(), None, None, field=key)
+		data = []
+		for result in results:
+			data.append(result)
+		
+		response = json.dumps(data)
+		
+		return response
+
+# This class handles requests for suggestions
+#
+#
 class query:
 
-	# This function handles GET requests for programs
+	# This function handles GET requests for programs and exercises
 	#
 	# Argument: collection
 	# Argument: key
@@ -119,19 +145,34 @@ class query:
 # Argument: collection
 # Argument: key
 # Argument: value
+# Argument: field
 # Yield: result
 #
 #
-def query_database(collection, key, value):
+def query_database(collection, key, value, field=None):
 
+	# First, build the projection
+	# If a field is specified for suggestion queries, only show that field
+	# Otherwise, show all fields other than the id
+	#
+	#
+	if field:
+		projection = { "_id" : False, field : True }
+	else:
+		projection = { "_id" : False }
+
+	# Query the database on the specified collection
+	# Iterate over the results and yield
+	#
+	#
 	if collection == "Programs":
 		if key and value:
 			regex_string = ".*" + value + ".*";
 			regex = re.compile(regex_string, re.IGNORECASE)
 			query = { key : regex }
-			results = db.Programs.find(query, { "_id" : False })
+			results = db.Programs.find(query, projection)
 		else:
-			results = db.Programs.find({}, { "_id" : False })
+			results = db.Programs.find({}, projection)
 
 		for result in results:
 			yield result
@@ -141,9 +182,9 @@ def query_database(collection, key, value):
 			regex_string = ".*" + value + ".*";
 			regex = re.compile(regex_string, re.IGNORECASE)
 			query = { key : regex }
-			results = db.Exercises.find(query, { "_id" : False })
+			results = db.Exercises.find(query, projection)
 		else:
-			results = db.Exercises.find({}, { "_id" : False })
+			results = db.Exercises.find({}, projection)
 
 		for result in results:
 			yield result
